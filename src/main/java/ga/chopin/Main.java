e73837fa24ac6b8d5095cf35a1fcc832d8715d83
Thomas <souahthomas@gmail.com>
2024-05-13 11:30:36 +0000

    test find method

--- src/main/java/ga/chopin/Main.java
+++ src/main/java/ga/chopin/Main.java
@@ -3,10 +3,9 @@ package ga.chopin;
 import ga.chopin.dao.Produit;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
-import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Persistence;
 
-import java.sql.Date;
+import java.util.List;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
@@ -15,12 +14,12 @@ public class Main {
 
 
         try (EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("demo");
-        EntityManager entityManager = entityManagerFactory.createEntityManager();) {
+        EntityManager entityManager = entityManagerFactory.createEntityManager()) {
 
             //démarage de la transaction
 
-            Produit p = new Produit("le lait nido", "10127l");
-            entityManager.getTransaction().begin();
+//            Produit p = new Produit("le lait nido", "10127l");
+//            Produit demo = null;
 
 
             try {
@@ -29,6 +28,15 @@ public class Main {
                    entityManager.getTransaction().commit();
 
                  */
+
+                /* initialisation d'un object par recherche en base  de donner d'un entityManager */
+//                demo = entityManager.find(Produit.class, 4);
+//                System.out.println(demo);
+                List<Produit> produits = entityManager.createQuery("select p from Produit p", Produit.class).getResultList();
+                for (Produit produit : produits) {
+                    System.out.println(produit);
+                }
+
             } catch (Exception e) {
                 entityManager.getTransaction().rollback();
                 e.printStackTrace();
